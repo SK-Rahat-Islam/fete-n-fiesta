501622353374d17f2604e8c8896fc7529f7048b0
SK-Rahat-Islam <81740329+SK-Rahat-Islam@users.noreply.github.com>
2022-11-18 01:14:25 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FETE-n-FIESTA
+ It's a event management system project using HTML,CSS,JAVASCRIPT AND PHP.
---
+++ login.php
@@ -0,0 +1,48 @@
+   <html>
+
+   <head>
+       <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>sign in</title>
+   </head>
+
+   <body>
+
+       <!-- header section starts  -->
+
+       <header class="header">
+
+           <a href="admin.php" class="logo">Fete n' Festa</a>
+
+           <nav class="navbar">
+               <a href="admin.php">home</a>
+               <a href="register.php">register</a>
+
+           </nav>
+
+           <div id="menu-bars" class="fas fa-bars"></div>
+
+       </header>
+       <!-- login section starts  -->
+       <link href="css/style.css" rel="stylesheet">
+
+       <div class="login">
+           <div class="form">
+               <form class="login-form">
+                   <input type="text" placeholder="username"/>
+                   <input type="password" placeholder="password" />
+                   <button>login</button>
+                   <p class="message">Not registered? <a href="register.php"><b> Create an account</b></a></p>
+               </form>
+           </div>
+       </div>
+
+
+   </body>
+
+   </html>
+
+
+
+   <!-- login section ends  -->
